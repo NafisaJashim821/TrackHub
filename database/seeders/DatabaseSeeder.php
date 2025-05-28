672b7ca5b7ce8d07d82812ddb466b5f4278df78e
NafisaJashim821 <149960527+NafisaJashim821@users.noreply.github.com>
2025-05-28 19:46:13 +0600

    Add files via upload

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\User;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UsersTableSeeder::class);
+		DB::table('users')->insert([
+            [
+                'name'          => 'Admin',
+                'email'         => 'admin@mail.com',
+                'password'      => bcrypt('codeastro.com'),
+                'created_at'    => date("Y-m-d H:i:s"),
+				'role'          => 'admin'
+            ],
+            [
+                'name'          => 'Staff',
+                'email'         => 'staff@mail.com',
+                'password'      => bcrypt('codeastro.com'),
+                'created_at'    => date("Y-m-d H:i:s"),
+				'role'          => 'staff'
+            ],
+        ]);
+    }
+}
